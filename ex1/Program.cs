ad92aa30e724b58f569398c6be4fa1986d237f6b
Vladimir <maostyle@mail.ru>
2022-07-09 00:00:11 +0300

    add program

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Console.WriteLine("Введите 1е число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2е число");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    Console.Write("max=");
+    Console.Write(a);
+}
+else 
+{
+    Console.Write("max=");
+    Console.Write(b);
+}
